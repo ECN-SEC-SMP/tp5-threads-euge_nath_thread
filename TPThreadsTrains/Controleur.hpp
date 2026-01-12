82b75d0a3fbb75d3d754b201e514fe6a1a22e6e0
Nandros <nathan.andre.etudiant@gmail.com>
2026-01-12 18:00:37 +0100

    Trains Q2.3

--- TPThreadsTrains/Controleur.hpp
+++ TPThreadsTrains/Controleur.hpp
@@ -13,27 +13,36 @@ class Controleur
 public:
     Controleur(int valeur_initiale) : val(valeur_initiale)
     {
+        this->count = 0;
+        this->waitingInA = false;
+        this->waitingInB = false;
     }
 
     bool controlinEnB(int numero)
     {
-        if ((this->val <= 0) && (this->val > -2))
+        if ((this->val == 0) && (this->val > -2) && ((this->waitingInA) || (this->count < 3)))
         {
+            this->count++;
+            waitingInB = false;
             this->val--;
             // std::cout << "inB Val = " << this->val << "\n";
             return true;
         }
+        waitingInB = true;
         return false;
     }
 
     bool controlinEnA(int numero)
     {
-        if ((this->val >= 0) && (this->val < 2))
+        if ((this->val == 0) && (this->val < 2) && ((this->waitingInA) || (this->count < 3)))
         {
+            this->count++;
+            waitingInA = false;
             this->val++;
             // std::cout << "inA Val = " << this->val << "\n";
             return true;
         }
+        waitingInA = true;
         return false;
     }
 
@@ -42,6 +51,10 @@ public:
         if (this->val > 0) {
             // std::cout << "outB Val = " << this->val << "\n";
             this->val--;
+
+            if (val == 0) {
+                this->count = 0;
+            }
             return true;
         }
         return false;
@@ -52,6 +65,10 @@ public:
         if (this->val < 0) {
             // std::cout << "outA Val = " << this->val << "\n";
             this->val++;
+
+            if (val == 0) {
+                this->count = 0;
+            }
             return true;
         }
         return false;
@@ -59,6 +76,9 @@ public:
 
 private:
     int val;
+    unsigned int count;
+    bool waitingInA;
+    bool waitingInB;
 };
 
 #endif // CONTROLEUR_HPP
